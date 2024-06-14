3cdf31780e04dbd2db96202bbd1e8f08e21171c9
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-06-14 11:27:42 +0100

    feat: complete Queue impl.

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.head || !this.tail) {
+            this.head = this.tail = node;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        let value = this.head.value;
+        this.head = this.head.next;
+
+        return value;
+    }
+
     peek(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        return this.head.value;
+    }
 }
