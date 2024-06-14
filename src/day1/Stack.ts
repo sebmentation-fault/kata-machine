84281f02e16ed098d5b8ba0b1d7d7d1598e06434
Sebastian Kjällgren <sebastian@kjallgren.com>
2024-06-14 11:22:02 +0100

    feat: complete Stack impl. & yarn install

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,47 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>,
+}
+
 export default class Stack<T> {
     public length: number;
-
-    
+    public head?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
     }
 
     push(item: T): void {
+        const node = { value: item } as Node<T>;
+        this.length++;
 
-}
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
     pop(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        let value = this.head.value;
+        this.head = this.head.prev;
+
+        return value;
+    }
+
     peek(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        return this.head.value;
+    }
 }
